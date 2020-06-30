a39ae4be85f2602bd5b4772b794eb6f9b34349f0
clueking1 <wcoan300@gmail.com>
2020-06-30 19:05:21 -0400

    added two conditional statements to check if all sign up fields are not empty and two see if pw and pw con match before new user is created

--- include/createUserInclude.php
+++ include/createUserInclude.php
@@ -4,5 +4,11 @@ spl_autoload_register(function ($class_name) {
 });
 include('../config/config.php');
 
-$createUser = new CreateUser($_POST["unSig"], $_POST["emailSig"], $_POST["pwSig"], $conn);
-echo $createUser->create();
+if ($_POST["unSig"]!== '' && $_POST["emailSig"] !== '' && $_POST["pwSig"] !== '') {
+    if($_POST["pwSig"] === $_POST["conPwSig"]) {
+        $createUser = new CreateUser($_POST["unSig"], $_POST["emailSig"], $_POST["pwSig"], $conn);
+        $createUser->create();
+    }
+} else {
+
+}
