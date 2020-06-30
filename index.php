7adb070031c3bed20de7c22d166f9f8b90964b15
clueking1 <wcoan300@gmail.com>
2020-06-30 15:01:11 -0400

    added autoload to index page and include config file on index php page

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
+<?php 
+    spl_autoload_register(function ($class_name) {
+        include './classes' . $class_name . '.php';
+    });
+    include('config/config.php')
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Plan It</title>
 </head>
 <body>
     <h2>Hi</h2>
