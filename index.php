229f3b63cde429819db0e8d59cad48fb0597b041
clueking1 <wcoan300@gmail.com>
2020-06-30 17:16:21 -0400

    added form js file to handle all AJAX calls

--- index.php
+++ index.php
@@ -24,5 +24,6 @@
    
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="./JS/modal.js"></script>
+    <script src="./JS/form.js"></script>
 </body>
 </html>
