42e98666c7854436364fb0993f1ba51514240e95
clueking1 <wcoan300@gmail.com>
2020-06-30 15:20:45 -0400

    added navbar to index php file

---
+++ addOns/navBar/navBar.php
@@ -0,0 +1,4 @@
+<nav>
+    <h2 class="titleNav">Plan It</h2>
+    <i class="fa fa-user" aria-hidden="true"></i>
+</nav>
--- index.php
+++ index.php
@@ -10,9 +10,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Plan It</title>
 </head>
 <body>
-    <h2>Hi</h2>
+    <?php include("addOns/navBar/navBar.php");?>
 </body>
 </html>
