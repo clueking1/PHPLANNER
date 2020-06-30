e2a621b5871ef943c5577868bd35f23ada73ee7e
clueking1 <wcoan300@gmail.com>
2020-06-30 14:52:50 -0400

    added db schema user table

---
+++ schema/db.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE planItDb;
+
+use planItDb;
+
+CREATE TABLE user (
+    id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    pw VARCHAR(50) NOT NULL,
+    PRIMARY KEY(id)
+);
