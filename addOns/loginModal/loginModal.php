9b454fef0f3a08dbf1953676327c413b3d769e08
clueking1 <wcoan300@gmail.com>
2020-06-30 16:09:01 -0400

    added model box template

---
+++ addOns/loginModal/loginModal.php
@@ -0,0 +1,13 @@
+<div class="modalContainer">
+    <div class="modalContent">
+        <h2 class="loginTitle">Login</h2>
+        <i class="fa fa-window-close" aria-hidden="true"></i>
+        <form method="post" class="loginForm" action="">
+            <input type="text" name="unLog" class="unLog" placeholder="username" />
+            <input type="password" name="pwLog" class="pwLog" placeholder="password" />
+            </br>
+            <button type="submit" class="logBut">Login</button>
+        </form> 
+            <a href="/PHPPLANNER">Sign Up</a>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -13,9 +13,11 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="./css/nav.css">
+    <link rel="stylesheet" href="./css/login.css">
     <title>Plan It</title>
 </head>
 <body>
     <?php include("addOns/navBar/navBar.php");?>
+    <?php include("addOns/loginModal/loginModal.php");?>
 </body>
 </html>
